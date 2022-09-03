d6f6bcb721a5de864ace6cef84fa29452afdaf96
Kenny <kenny@3rices.com>
2022-09-03 15:44:19 +0800

    clean up index tsx'

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import { Routes, Route } from 'react-router-dom'
-
-// import { ContactList } from "./components/ContactList";
-import { DummyComponent } from './components/DummyComponent'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="contact" element={<DummyComponent title="contact" />} />
-          <Route
-            path="contact:id"
-            element={<DummyComponent title="chaaracter" />}
-          />
-        </Route>
-        <Route path="*" element={<DummyComponent title="Not Found" />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
