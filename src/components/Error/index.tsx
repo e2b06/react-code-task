c33da0127f9649cafed36236c8fb4df62e833338
Kenny <kenny@3rices.com>
2022-09-04 00:35:56 +0800

    Add error component

---
+++ src/components/Error/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Error: React.FC<{}> = () => {
+  return (
+    <div className="w-full flex items-center justify-center bg-sky-500">
+      <h1>Something Wrong...</h1>
+    </div>
+  )
+}
