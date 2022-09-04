f64670400c09ba11ca35001cbccaa512b7fa35da
Kenny <kenny@3rices.com>
2022-09-04 20:32:12 +0800

    update style

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Error: React.FC<{}> = () => {
   return (
-    <div className="w-full flex items-center justify-center bg-sky-500">
+    <div className="flex items-center justify-center w-full h-full bg-sky-500">
       <h1>Something Wrong...</h1>
     </div>
   )
