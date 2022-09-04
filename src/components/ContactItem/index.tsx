ceed2c077226732b6337fce29be89fb4c925ee05
Kenny <kenny@3rices.com>
2022-09-04 17:38:22 +0800

    update contact item structure

--- src/components/ContactItem/index.tsx
+++ src/components/ContactItem/index.tsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-interface ContactItemProps {
-  image: string
-  name: string
-  type: 'link' | 'header'
-  species?: string
-  id?: number
-}
-
 const style = {
   header: {
     icon: 'w-auto max-h-[50px] mr-4 rounded-full md:max-h-[150px] md:mr-7',
@@ -20,27 +12,35 @@ const style = {
   },
 }
 
-export const ContactItem: React.FC<ContactItemProps> = ({
-  image,
-  name,
-  species,
-  id,
-  type,
-}) => {
+interface ContactItemProps {
+  contact: any
+  type: 'header' | 'link'
+}
+
+export interface ContactRouterState {
+  contact: any
+}
+
+export const ContactItem: React.FC<ContactItemProps> = ({ contact, type }) => {
   const commonClass = 'component-contact-item flex items-center'
 
   const content = (
     <>
-      <img src={image} alt="" className={style[type].icon} />
+      <img src={contact.image} alt="" className={style[type].icon} />
       <div className="mr-5">
-        <p className={style[type].name}>{name}</p>
-        {species && <p>{species}</p>}
+        <p className={style[type].name}>{contact.name}</p>
+        {type === 'link' && <p>{contact.species}</p>}
       </div>
     </>
   )
 
   return type === 'link' ? (
-    <Link to={`/contact/${id}`} key={id} className={commonClass}>
+    <Link
+      to={`/contact/${contact.id}`}
+      key={contact.id}
+      className={commonClass}
+      state={{ contact } as ContactRouterState}
+    >
       {content}
     </Link>
   ) : (
