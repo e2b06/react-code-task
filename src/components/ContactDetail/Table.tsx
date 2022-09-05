33e01fd9027df5b015e3862acbef6799526ecd43
Kenny <kenny@3rices.com>
2022-09-05 20:48:58 +0800

    update style

--- src/components/ContactDetail/Table.tsx
+++ src/components/ContactDetail/Table.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
 interface TableProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export const TableSpan: React.FC<{ text: string; isHeader?: boolean }> = ({
@@ -10,24 +10,24 @@ export const TableSpan: React.FC<{ text: string; isHeader?: boolean }> = ({
 }) => {
   return (
     <p
-      className={`grid-span-1 text-center${
-        isHeader ? " font-bold text-l mb-3" : ""
+      className={`grid-span-1 text-center break-words${
+        isHeader ? ' font-bold text-l mb-3' : ''
       }`}
     >
       {text}
     </p>
-  );
-};
+  )
+}
 
 export const Table: React.FC<TableProps> = ({ children }) => {
-  const tableHeaders = ["Name", "Air Date", "Episode", "Created Date"];
+  const tableHeaders = ['Name', 'Air Date', 'Episode', 'Created Date']
 
   return (
     <div className="grid grid-cols-4 gap-3 items-center md:gap-5">
       {tableHeaders.map((header, index) => {
-        return <TableSpan text={header} isHeader={true} key={index} />;
+        return <TableSpan text={header} isHeader={true} key={index} />
       })}
       {children}
     </div>
-  );
-};
+  )
+}
