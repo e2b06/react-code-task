5e17c7c72bd3cf2684267b521c0afadc3e8eef89
Kenny <qmm6625@gmail.com>
2022-09-05 11:46:38 +0800

    modify wording

--- src/components/ContactList/SelectItem.tsx
+++ src/components/ContactList/SelectItem.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export type OnSearchChangeType = (
-  event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
-) => void
+  event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+) => void;
 
 export const SelectItem: React.FC<{
-  name: string
-  onSearchChange: OnSearchChangeType
-  value: string
-  options: string[]
+  name: string;
+  onSearchChange: OnSearchChangeType;
+  value: string;
+  options: string[];
 }> = ({ name, onSearchChange, value, options }) => {
   return (
     <select
@@ -17,14 +17,14 @@ export const SelectItem: React.FC<{
       onChange={onSearchChange}
       value={value}
     >
-      <option value="">select an option</option>
+      <option value="">Select an option</option>
       {options.map((option, index) => {
         return (
           <option value={option.toLowerCase()} key={index}>
             {option}
           </option>
-        )
+        );
       })}
     </select>
-  )
-}
+  );
+};
